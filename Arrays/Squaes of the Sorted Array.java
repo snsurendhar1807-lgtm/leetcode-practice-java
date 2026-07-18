7a819d6e27cf7e4b438444ddd3cd9831639292e5
SURENDHAR S N <s.n.surendhar1807@gmail.com>
2026-07-18 10:15:17 +0530

    Squaes of the Sorted Array.java

---
+++ Arrays/Squaes of the Sorted Array.java
@@ -0,0 +1,13 @@
+//LeetCode 977 -  Squaes of the Sorted Array
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int [] res = new int[n];
+        for(int i=0; i<n;i++){
+            res[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(res);
+        return res;
+    }
+}
