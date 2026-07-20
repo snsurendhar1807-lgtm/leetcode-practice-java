27223345d5568a571a35e82190e78fc70871172b
SURENDHAR S N <s.n.surendhar1807@gmail.com>
2026-07-20 21:34:00 +0530

    Richest Costumer Wealth.java

---
+++ Arrays/Richest Costumer Wealth.java
@@ -0,0 +1,15 @@
+//LeetCode 1672 - Richest Costumer Wealth
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int rich = 0;
+        for (int[] i : accounts) {
+            int sum = 0;
+            for (int money : i) {
+                sum += money;
+            }
+            rich = Math.max(rich, sum);
+        }
+        return rich;
+    }
+}
